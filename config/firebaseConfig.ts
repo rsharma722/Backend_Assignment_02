d3f82fabe5073d8ae3f73eefd6cc2d350950d352
Ravdeep Sharma <rsharma1.academic@rrc.ca>
2025-10-15 00:00:32 -0500

    feat(project): initialize project structure with required folders and starter files

--- config/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
-import { getFirestore, Firestore } from "firebase-admin/firestore";
-import * as serviceAccount from "../Service_Key.json";
-
-initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
-});
-
-export const db: Firestore = getFirestore();
